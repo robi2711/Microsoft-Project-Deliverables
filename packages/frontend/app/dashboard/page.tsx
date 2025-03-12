f774e4e6358cc120b32ff91d961599712869b615
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2025-03-12 16:23:29 +0000

    Branch for Version 3
    Added Dashboard page and dashboard components, will develop soon

---
+++ packages/frontend/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// This will become the Dashboard page - pasted from scanner for now.
+"use client"
+
+import ScannerPage from "@/components/pages/ScannerPage"
+
+export default function Scanner() {
+    return (
+        <ScannerPage />
+    )
+}
