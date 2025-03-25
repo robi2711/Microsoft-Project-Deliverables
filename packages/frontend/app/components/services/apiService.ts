a4278f5573455d0c9ce7c45924824f2696bc8a24
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-03-25 21:17:58 +0000

    Update apiService.ts

--- packages/frontend/app/components/services/apiService.ts
+++ packages/frontend/app/components/services/apiService.ts
@@ -1,10 +1,8 @@
 // This file is used to create an axios instance that will be used to make requests to the backend.
 // The baseURL is the URL of the backend that we want to make requests to.
 // All you have to do is import this file and use the api object to make requests to the backend. See the TmpButton component for an example of how to use this api object.
-import dotenv from 'dotenv';
 import axios from "axios";
 
-dotenv.config()
 
 console.log('Backend URL:', process.env.BACKEND_URL);
 
