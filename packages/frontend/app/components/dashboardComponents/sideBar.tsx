ca8560867655c130c3401301ecb815188649cca4
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-03-25 20:41:23 +0000

    Fixed ESlint errors

--- packages/frontend/app/components/dashboardComponents/sideBar.tsx
+++ packages/frontend/app/components/dashboardComponents/sideBar.tsx
@@ -4,9 +4,15 @@
 // importing necessary modules
 import { Box, Typography, MenuItem, FormControl, Select, Tab, Link } from "@mui/material"
 import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
+import Image from 'next/image';
 
 {/* We need arguments relating to active tabs so we can highlight the active */}
-export default function SideBar({ setActiveTab, activeTab }) {
+interface SideBarProps {
+    setActiveTab: (tab: string) => void;
+    activeTab: string;
+}
+
+export default function SideBar({ setActiveTab, activeTab }: SideBarProps) {
     return(
         <Box
             sx={{
@@ -19,8 +25,8 @@ export default function SideBar({ setActiveTab, activeTab }) {
         >
             {/* adding a logo and title to the sidebar within it's own box */}
             <Box sx={{ display: "flex", alignItems: "center"}}>
-                <img src="https://raw.githubusercontent.com/robi2711/Microsoft-Project-Deliverables/refs/heads/version-3-frontend/favicon.ico"
-                     style={{ width: 24, height: 24, marginRight: 8 }} />
+                <Image src="https://raw.githubusercontent.com/robi2711/Microsoft-Project-Deliverables/refs/heads/version-3-frontend/favicon.ico"
+                     style={{ width: 24, height: 24, marginRight: 8 }} alt={""}/>
                 <Typography variant="h5" sx={{ color: "white", textAlign: "center" }}>
                     Deliverables
                 </Typography>
@@ -60,8 +66,8 @@ export default function SideBar({ setActiveTab, activeTab }) {
             <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 2, width: "100%" }}>
                 <Box sx={{ width: "80%", height: "100px", bgcolor: "white"}}>
                     {/* TODO: Google Maps API goes here - gotta look into that */}
-                    <img src="https://raw.githubusercontent.com/robi2711/Microsoft-Project-Deliverables/refs/heads/version-3-frontend/SampleImage.png"
-                         style={{ width: "100%", height: "100%"}}/>
+                    <Image src="https://raw.githubusercontent.com/robi2711/Microsoft-Project-Deliverables/refs/heads/version-3-frontend/SampleImage.png"
+                         style={{ width: "100%", height: "100%"}} alt={""}/>
                 </Box>
             </Box>
 
