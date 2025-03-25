033ff86ced207d4dbff54ce830c4bfb6285446ab
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-03-25 18:11:09 +0000

    DEYPLOY backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -9,7 +9,7 @@ import cors from 'cors';
 import dbRoutes from "@/routes/dbRoutes";
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json()); // This is used to parse the data that the frontend sends to the backend
