93bfe8c9dbef7565cbd76e9b0aae37a05e393411
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-04-12 03:18:50 +0100

    Added multiple cors origins

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,4 +1,9 @@
 import 'module-alias/register';
+import moduleAlias from 'module-alias';
+import * as path from 'path';
+moduleAlias({
+	base: path.resolve(__dirname, '..') // points to the folder with the relevant package.json
+});
 import express from 'express';
 import cors from 'cors';
 import dbRoutes from "@/routes/dbRoutes";
@@ -7,12 +12,11 @@ import session from "express-session";
 
 import ocrRoutes from '@/routes/ocrRoutes';
 
-
 const PORT = process.env.PORT || 3001;
-
 
 const app = express();
 app.use(express.json()); // This is used to parse the data that the frontend sends to the backend
+app.use(express.urlencoded()); // This allows for body to be parsed when testing with POST forms in Postman!
 app.use(session({
 	secret: 'v',
 	resave: true,
@@ -20,17 +24,29 @@ app.use(session({
 	cookie: { secure: false }
 })); // Backend session management
 
-app.use(cors({
-	origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-	credentials: true
-})); // This is used to allow the frontend to make requests to the backend
+const allowedOrigins = [
+	process.env.FRONTEND_URL || 'http://localhost:3000',
+	'https://api.twilio.com'
+];
 
-
-
-//app.use("/db", dbRoutes); // This is the route that the frontend will use to make requests to the backend.
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			if (!origin) {
+				return callback(null, true);
+			}
+			if (allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error('Not allowed by CORS'));
+			}
+		},
+		credentials: true // Allow cookies and credentials
+	})
+);
 app.use("/auth", authRoutes);
-
 app.use("/db", dbRoutes); // This is the route that the frontend will use to make requests to the backend.
+//app.use("/whatsapp", twilioRoutes)
 app.use('/ocr', ocrRoutes); // Route for OCR
 
 app.listen(PORT, () => {
