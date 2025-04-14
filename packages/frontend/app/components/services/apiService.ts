ea34dca90cc45d9c7bad942371d0ba8bbce7e1b1
Robert Stefan Gontaru <robert.gontaru@outlook.com>
2025-04-14 17:55:40 +0100

    deploy

--- packages/frontend/app/components/services/apiService.ts
+++ packages/frontend/app/components/services/apiService.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-	baseURL: "http://localhost:3001",
+	baseURL: "https://deliverablesbackend-gncxepeyepaydacm.uksouth-01.azurewebsites.net",
 	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json',
